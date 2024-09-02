9d5d4b0e1ec32c536eab4456c2e8ac59b7cf6dc1
harakki <124341753+harakki@users.noreply.github.com>
2024-09-02 18:02:55 +0700

    Complete lab 2

--- lab02/main.c
+++ lab02/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <sys/sysinfo.h>
 
-int main (void) {
-    return 0;
+int main(void) {
+  struct sysinfo info;
+  int func_return = 0;
+
+  // Get info about RAM and SWAP
+  func_return = sysinfo(&info);
+
+  // Exit program if any error returned
+  if (func_return != 0) {
+    perror("sysinfo:EFAULT");
+    return 1;
+  }
+
+  // Output collected data
+  printf("%16s %16s %16s %16s %16s %16s\n", "RAM-SIZE", "FREE-RAM",
+         "SHARED-RAM", "BUFFERED-RAM", "SWAP-SIZE", "FREE-SWAP");
+
+  printf("%16lu %16lu %16lu %16lu %16lu %16lu\n", info.totalram, info.freeram,
+         info.sharedram, info.bufferram, info.totalswap, info.freeswap);
+
+  return 0;
 }
